5929f2737f673942625cb44f2d5eb67c62abc0d0
IDCDEV03 <idcdev03@iddrives.co.th>
2025-09-15 11:23:00 +0700

    update15-09-68

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,6 +38,8 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             // \App\Http\Middleware\SetLanguage::class,
+
+            \App\Http\Middleware\PreventBackHistory::class,
         ],
 
         'api' => [
